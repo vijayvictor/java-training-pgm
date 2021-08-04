6ae443ed41a0b5dcb5080e9ccf1a4190798d97fa
vijayvictor <84435870+vijayvictor@users.noreply.github.com>
2021-08-04 21:21:47 +0530

    Add files via upload

---
+++ SERVLET/First/Hello.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+ class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Hello() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+
+	/*
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h3>Hello Servlet</h3>");
+		out.print("</body></html>");
+	}
+
+}
+
