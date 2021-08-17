6baeec2fb2dca35df9e4dcbc6e23c9e0c7c1b807
vijayvictor <84435870+vijayvictor@users.noreply.github.com>
2021-08-17 11:18:31 +0530

    Add files via upload

---
+++ SPRING BOOT/BANKING APPLICATION/Src/main/java/com/service/BankServiceImp.java
@@ -0,0 +1,65 @@
+package com.BankingApplication.Service;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.BankingApplication.EntitiesPojo.Account;
+import com.BankingApplication.EntitiesPojo.Operation;
+
+/**
+ * Description of file IBankService.java
+ * 
+ * @author 
+ *
+ *         This interface provide the functional requirement of the bank
+ *         application <br>
+ */
+
+@Service
+
+public interface BankServiceImp {
+
+	public Account getAccountById(String accountId);
+
+	/**
+	 * It is used to add the specified amount at the discount of the specified
+	 * account <br>
+	 * 
+	 * @param accountId
+	 * @param amount
+	 */
+	public void payToAccount(String accountId, double amount);
+
+	/**
+	 * This method is used to remove the specified amount from the specified
+	 * account
+	 * 
+	 * @param accountId
+	 * @param amount
+	 */
+	public void removeFromAccount(String accountId, double amount);
+
+	/**
+	 * This method is used to make a transfer between tw@Override
+	o account.
+	 * 
+	 * @param accountOriginId
+	 * @param accountDestId
+	 * @param amount
+	 */
+
+	public void transfer(String accountOriginId, String accountDestId, double amount);
+
+	/**
+	 * This method is used to get operation of the specified account by page
+	 * 
+	 * @param accountId
+	 * @param page
+	 * @param size
+	 * @return
+	 */
+
+	public Page<Operation> getAccountOperationByPage(String accountId, int page, int size);
+
+}
